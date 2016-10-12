cf09304ddebd62b07422b6a0a92965211359db40
sebastian101 <sebastianduq.80@gmail.com>
2016-10-11 21:09:00 -0500

    11/10/2016
    11/10/2016

--- JAVA Fundamentals 8/D83527GC10_labs/labs/11-ArraysLoopsDates/Exercises/Exercise2/ex11_2_exercise/ShoppingCart.java
+++ JAVA Fundamentals 8/D83527GC10_labs/labs/11-ArraysLoopsDates/Exercises/Exercise2/ex11_2_exercise/ShoppingCart.java
@@ -6,9 +6,19 @@ public class ShoppingCart {
         String name;
         int age;
 
-        // Parse the args array to populate name and age.  
+        // Parse the args array to populate name and age. 
+	
+	
 	// Print an error message if fewer than 2 args are passed in.
-        
+        if (args.length != 2){
+	System.out.println("2 arguments are required");
+
+	}
+	else{
+	name = args[0];
+	age = Integer.parseInt(args[1]);
+	System.out.println("Nombre: " + name + " Edad: " + age );
+	}
         
     }
 }
