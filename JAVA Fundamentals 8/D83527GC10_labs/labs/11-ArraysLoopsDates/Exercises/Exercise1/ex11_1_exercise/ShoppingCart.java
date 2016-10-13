e0912aebab6daf302e40707b6d670c1035e5d512
Sebastian Duque <sebastian.duque@payulatam.com>
2016-10-12 20:04:16 -0500

    12/10/2016
    12/10/2016

--- JAVA Fundamentals 8/D83527GC10_labs/labs/11-ArraysLoopsDates/Exercises/Exercise1/ex11_1_exercise/ShoppingCart.java
+++ JAVA Fundamentals 8/D83527GC10_labs/labs/11-ArraysLoopsDates/Exercises/Exercise1/ex11_1_exercise/ShoppingCart.java
@@ -1,18 +1,25 @@
 package ex11_1_exercise;
 
 // import statements here:
-
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class ShoppingCart {
     public static void main(String[] args){
 	// Declare a LocalDateTime object, orderDate
-
+	LocalDate orderDate;
+	
         
 	// Initialize the orderDate to the current date and time. Print it.
-
+	orderDate = LocalDate.now();
+	System.out.println("The order Date is: " + orderDate);
 
 	// Format orderDate using ISO_LOCAL_DATE; Print it.
+	orderDate.format(DateTimeFormatter.ISO_LOCAL_DATE);
+	System.out.println("This is the order date with format ISO: "+orderDate);
 
+		
 
     }
 }
--- JAVA Fundamentals 8/D83527GC10_labs/labs/11-ArraysLoopsDates/Exercises/Exercise2/ex11_2_exercise/ShoppingCart.java
+++ JAVA Fundamentals 8/D83527GC10_labs/labs/11-ArraysLoopsDates/Exercises/Exercise2/ex11_2_exercise/ShoppingCart.java
@@ -7,8 +7,16 @@ public class ShoppingCart {
         int age;
 
         // Parse the args array to populate name and age.  
+	name = args[0];	
+	age = Integer.parseInt(args[1]);
+
 	// Print an error message if fewer than 2 args are passed in.
-        
+	if(args.length != 2){
+		System.out.println("Please enter at least 2 values");
+	}
+	else
+	System.out.println("Name: " + name + " " + "Age: " + age );  
+	      
         
     }
 }
